28198f8dac5aa92e5bfa32b02ca2e1e0587a5f1f
MatiasCaceres <matiasagustincaceres@hotmail.com>
2022-05-14 10:44:18 -0300

    Create react app

---
+++ luna_magica/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Me decis tus signo bb?</h1>
+    </div>
+  );
+}
+
+export default App;
